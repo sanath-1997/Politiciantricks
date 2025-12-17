dff10c7c76f5a7e3d6cec855f982225b9ab0f40f
Sanath K S <sanathselvakumaran@gmail.com>
2025-12-17 06:01:35 +0000

    Increase the padding here.

--- src/components/payment-modal.tsx
+++ src/components/payment-modal.tsx
@@ -154,8 +154,8 @@ export function PaymentModal() {
                 className="
                   w-full
                   max-w-full
-                  px-3
-                  sm:px-4
+                  px-6
+                  py-4
                   text-sm
                   sm:text-base
                   leading-snug
